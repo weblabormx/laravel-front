71d35516f8dcdc0a25b6d3e6057768d02fe07211
Carlos Escobar <carlosescobar@weblabor.mx>
2020-01-28 00:04:54 -0600

    make files equals

--- resources/views/elements/actions.blade.php
+++ resources/views/elements/actions.blade.php
@@ -1,7 +1,5 @@
 @if(count($front->getActions())>0)
     @foreach($front->getActions() as $action)
-	    @if($action->show_button)
-	        <a href="{{$front->base_url}}/{{$object->getKey()}}/action/{{$action->slug}}" class="btn btn-primary rounded-pill" style="{{$action->getStyle()}}">{!! $action->button_text !!}</a>
-        @endif
+        <a href="{{$front->base_url}}/{{$object->getKey()}}/action/{{$action->slug}}" class="btn btn-primary rounded-pill" style="{{$action->getStyle()}}">{!! $action->button_text !!}</a>
     @endforeach
 @endif
