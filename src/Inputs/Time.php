3f523d9aa35bb859ab1e32f79287d8104d7eb13c
Carlos Escobar <carlosescobar@weblabor.mx>
2020-02-22 16:44:48 -0600

    add time input

---
+++ src/Inputs/Time.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace WeblaborMx\Front\Inputs;
+
+class Time extends Input
+{
+	public $pattern = '([01]?[0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]';
+	public $input_type = 'text';
+
+	public function form()
+	{
+		$this->attributes['pattern'] = $this->pattern;
+		$type = $this->input_type;
+		return \Form::$type($this->column, $this->default_value, $this->attributes);
+	}
+
+	public function ignoreSeconds()
+	{
+		$this->pattern = '([01]?[0-9]|2[0-3]):[0-5][0-9]';
+		$this->input_type = 'time';
+		return $this;
+	}
+}
