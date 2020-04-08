c80643dba278296f13f7ba9e6053172367d61393
Carlos Escobar <carlosescobar@weblabor.mx>
2020-04-08 11:43:35 -0500

    update belongs to, not extending has many, and removing create button

--- src/Inputs/BelongsToMany.php
+++ src/Inputs/BelongsToMany.php
@@ -2,8 +2,43 @@
 
 namespace WeblaborMx\Front\Inputs;
 
-class BelongsToMany extends HasMany
+use Illuminate\Support\Str;
+use WeblaborMx\Front\Traits\InputWithActions;
+use WeblaborMx\Front\Traits\InputWithLinks;
+use WeblaborMx\Front\Traits\InputRelationship;
+
+class BelongsToMany extends Input
 {
+	use InputWithActions, InputWithLinks, InputRelationship;
+	
+	public $is_input = false;
+	public $show_on_edit = false;
+	public $show_on_create = false;
+	public $show_on_index = false;
+
+	public function __construct($front, $title = null, $column = null, $source = null)
+	{
+		$front = 'App\Front\\'.$front;
+		$this->front = new $front($source);
+		$this->column = $column;
+		$this->source = $source;
+		if(!is_null($title)) {
+			$this->title = $title;
+			$this->relationship = Str::snake(Str::plural($this->title));
+		} else {
+			$this->title = $this->front->plural_label;
+			$this->relationship = Str::snake(Str::plural(class_basename(get_class($this->front))));
+		}
+		
+		$this->masive_edit_link = '';
+	}
+
+	public static function make($title = null, $column = null, $extra = null) 
+	{
+		$source = session('source');
+		return new static($title, $column, $extra, $source);	
+	}
+
 	public function setResource($resource)
 	{
 		// Get column name
@@ -12,43 +47,43 @@ class BelongsToMany extends HasMany
 			$class = $resource->getModel();
 			$model = new $class;
 			$relation_function = $model->$relation();
-
 			$this->column = $relation_function->getForeignPivotKeyName();
 		}
 
 		$base_url = $this->getBaseUrl($resource, $relation_function);
-		
-		// Hide column of the resource by default if there isnt any hide columns
-		if(!isset($this->hide_columns)) {
-			$this->front = $this->front->hideColumns($this->getColumnsToHide());
+		return parent::setResource($resource);
+	}
+
+	public function getValue($object)
+	{
+		// Get results
+		$pagination_name = $this->relationship.'_page';
+		$result = $this->getResults($object);
+		if(get_class($result) != 'Illuminate\Support\Collection') {
+			$result = $result->paginate($this->front->pagination, ['*'], $pagination_name);
 		}
 
-		// If any link has been set so add to select by default the relationhip
-		if(!isset($this->create_link_accessed)) {
-			$this->setCreateLink(function($link) use ($resource, $base_url) {
-				return $link.'?'.$base_url.'&relation_front='.class_basename(get_class($resource)).'&relation_id='.$resource->object->getKey().'&redirect_url='.$resource->base_url.'/'.$resource->object->getKey();
-			});
-		}
+		$front = $this->front;
+		$edit_link = $this->edit_link;
+		$show_link = $this->show_link;
 
-		// The same for edit
-		if(!isset($this->edit_link_accessed)) {
-			$this->setEditLink(function($link) use ($resource) {
-				return $link.'?relation_front='.class_basename(get_class($resource)).'&relation_id='.$resource->object->getKey();;
-			});
-		}
+		return view('front::crud.partial-index', compact('result', 'front', 'pagination_name', 'edit_link', 'show_link'))->render();
+	}
 
-		// The same for show
-		if(!isset($this->show_link_accessed)) {
-			$this->setShowLink(function($link) use ($resource) {
-				return $link.'?relation_front='.class_basename(get_class($resource)).'&relation_id='.$resource->object->getKey();
-			});
-		}
+	public function getResults($object)
+	{
+		// Get objects
+		$relationship = $this->relationship;
+		$objects = $object->$relationship()->with($this->with);;
+		return $objects;
+	}
 
-		// Hide columns
-		if(isset($this->hide_columns)) {
-			$this->front = $this->front->hideColumns($this->hide_columns);
-		}
-		$this->resource = $resource;
-		return $this;
+	/*
+	 * Shared functions
+	 */
+
+	public function getBaseUrl($resource, $relation_function)
+	{
+		return $this->column.'='.$resource->object->getKey();
 	}
 }
