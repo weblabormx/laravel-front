eb8c056db7a62f61fcb5689da893c78ffd97d719
Carlos Escobar <carlosescobar@weblabor.mx>
2020-05-01 23:27:39 -0500

    styles updates

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,7 +2,7 @@
 
 @section('after-nav')
 
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark p-0">
         <div class="container">
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarFrontMenu" aria-controls="navbarFrontMenu" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
