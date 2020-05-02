9dd6f4a6692ee937ece3d233f0e6133fd2550ff1
Carlos Escobar <carlosescobar@weblabor.mx>
2020-05-01 23:21:19 -0500

    add information to config

--- config/front.php
+++ config/front.php
@@ -2,8 +2,41 @@
 
 return [
 
+	/*
+    |--------------------------------------------------------------------------
+    | Model's Folder
+    |--------------------------------------------------------------------------
+    |
+    | This is the folder location where the models are located, used when  
+    | creating resources with the commands.
+    |
+    */
+
     'models_folder' => 'App',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Base Url
+    |--------------------------------------------------------------------------
+    |
+    | This value is used when creating a new resource, it will add it automatically
+    | on the base_url attribute
+    |
+    */
+
     'default_base_url' => '/admin',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Search Filter
+    |--------------------------------------------------------------------------
+    |
+    | The Apply() function on this filter will be used when making a search
+    | for a Front Resource. Used for autocomplete inputs and when adding 
+    | searchable() to relationship functions.
+    |
+    */
+
     'default_search_filter' => App\Front\Filters\SearchFilter::class,
 
 ];
