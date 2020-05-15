d988af6df4de0a8fcb3d618a14aafd232478a984
Carlos Escobar <carlosescobar@weblabor.mx>
2020-05-15 17:25:03 -0500

    password shouldn’t be shown on index or show

--- src/Inputs/Password.php
+++ src/Inputs/Password.php
@@ -4,6 +4,9 @@ namespace WeblaborMx\Front\Inputs;
 
 class Password extends Input
 {
+	public $show_on_index = false;
+	public $show_on_show = false;
+
 	public function form()
 	{
 		return \Form::password($this->column, $this->attributes);
