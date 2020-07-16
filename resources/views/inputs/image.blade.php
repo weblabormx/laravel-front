c7bc307bcf5b84716a7c3cd36b75320435ea5592
Carlos Escobar <carlosescobar@weblabor.mx>
2020-07-16 12:17:21 -0500

    update

--- resources/views/inputs/image.blade.php
+++ resources/views/inputs/image.blade.php
@@ -1,5 +1,5 @@
-@if(!is_null($thumb))
+@isset($thumb)
 	<a href="{{$original}}" data-type="popup" data-popup-type="image">
 		<img src="{{$thumb}}" style="max-width:100%" />	
 	</a>
-@endif
+@endisset
