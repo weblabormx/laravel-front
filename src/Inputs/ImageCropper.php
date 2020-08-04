c98659946184d3bd771a9764ac04fd56d37bf4f8
Carlos Escobar <carlosescobar@weblabor.mx>
2020-08-04 11:44:05 -0500

    image cropper can have handler

--- src/Inputs/ImageCropper.php
+++ src/Inputs/ImageCropper.php
@@ -5,17 +5,29 @@ namespace WeblaborMx\Front\Inputs;
 class ImageCropper extends Input
 {
 	public $image;
+	public $handler;
 	public $ratio = null;
 	public $width = 500;
 
 	public function form()
 	{
-		$id = 'image-to-crop-'.rand(1, 10);
+		$rand = rand(1, 100);
+		$id = 'image-to-crop-'.$rand;
+		$function = "handler{$rand}";
 		$this->attributes['data-type'] = 'cropper';
 		$this->attributes['data-image'] = $id;
 		$this->attributes['data-width'] = $this->width;
 		$this->attributes['data-ratio'] = $this->ratio;
-		return '<img src="'.$this->image.'" id="'.$id.'" style="max-width: none !important;" />'.\Form::hidden($this->column, $this->default_value, $this->attributes);
+		if(isset($this->handler)) {
+			$this->attributes['data-handler'] = $function;
+		}
+		$html = '<img src="'.$this->image.'" id="'.$id.'" style="max-width: none !important;" />';
+		$html .= \Form::hidden($this->column, $this->default_value, $this->attributes);
+		if(isset($this->handler)) {
+			$handler = $this->handler;
+			$html .= '<script type="text/javascript">function '.$function.'(c) { '.$handler().' }</script>';
+		}
+		return $html;
 	}
 
 	public function setImage($image)
@@ -33,6 +45,12 @@ class ImageCropper extends Input
 	public function setRatio($ratio)
 	{
 		$this->ratio = $ratio;
+		return $this;
+	}
+
+	public function setHandler($handler)
+	{
+		$this->handler = $handler;
 		return $this;
 	}
 }
