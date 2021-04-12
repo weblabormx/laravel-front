2df64fcffc4317bba8788266171c833c603cb11d
Carlos Escobar <carlos@xmlteam.com>
2021-04-12 00:59:31 -0500

    Is active instead of active for if

--- src/FrontServiceProvider.php
+++ src/FrontServiceProvider.php
@@ -114,7 +114,7 @@ class FrontServiceProvider extends ServiceProvider
             return "<?php if(request()->is($route.'/*') || request()->is($route)) echo 'active';?>";
         });
 
-        Blade::if('active', function ($route) {
+        Blade::if('is_active', function ($route) {
             return request()->is($route.'/*') || request()->is($route);
         });
 
