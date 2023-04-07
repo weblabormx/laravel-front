569a4aac67ff23d462dc2b5595fa4733411e85ee
Carlos Escobar <skalero01@gmail.com>
2023-04-07 16:09:34 -0600

    add decimals option to number to round

--- src/Inputs/Number.php
+++ src/Inputs/Number.php
@@ -4,9 +4,37 @@ namespace WeblaborMx\Front\Inputs;
 
 class Number extends Input
 {
+	public $decimals = null;
+
 	public function form()
 	{
-		$this->attributes['step'] = 'any';
+		$this->attributes['step'] = $this->getStep();
 		return \Form::number($this->column, $this->default_value, $this->attributes);
+	}
+
+	public function decimals($decimals)
+	{
+		$this->decimals = $decimals;
+		return $this;
+	}
+
+	private function getStep()
+	{
+		if(is_null($this->decimals)) {
+			return 'any';
+		}
+		if($this->decimals==0) {
+			return 1;
+		}
+		return 1/pow(10, $this->decimals);
+	}
+
+	public function getValue($object)
+	{
+		$value = parent::getValue($object);
+		if(is_null($this->decimals) || !is_numeric($value) || !is_numeric($this->decimals)) {
+			return $value;
+		}
+		return round($value, $this->decimals);
 	}
 }
