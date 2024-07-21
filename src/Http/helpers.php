ce9cd708a0e927f72628cbf4d16152dfacc258b7
Carlos Escobar <skalero01@gmail.com>
2024-07-20 18:23:23 -0600

    fix error with https on delete

--- src/Http/helpers.php
+++ src/Http/helpers.php
@@ -41,11 +41,27 @@ function getButtonByName($name, $front = null, $object = null)
     $config = config('front.buttons.'.$name);
     $extra = '';
     if($name == 'delete') {
-        $extra = "data-type='confirm' title='".__('Delete')."' data-info='".__('Do you really want to remove this item?')."' data-button-yes='".__('Yes')."' data-button-no='".__('No')."' data-action='".url($front->getBaseUrl().'/'.$object->getKey())."' data-redirection='".url($front->getBaseUrl())."' data-variables='{ \"_method\": \"delete\", \"_token\": \"".csrf_token()."\" }'";
+        $remove_url = fixUrl(url($front->getBaseUrl().'/'.$object->getKey()));
+        $extra = "data-type='confirm' title='".__('Delete')."' data-info='".__('Do you really want to remove this item?')."' data-button-yes='".__('Yes')."' data-button-no='".__('No')."' data-action='".$remove_url."' data-redirection='".url($front->getBaseUrl())."' data-variables='{ \"_method\": \"delete\", \"_token\": \"".csrf_token()."\" }'";
     }
     return Button::make($config['name'])
         ->setIcon($config['icon'])
         ->setExtra($extra)
         ->setType($config['type'])
         ->setClass($config['class']);
+}
+
+function fixUrl($url)
+{
+    if (
+        isset($_SERVER['HTTPS']) &&
+        ($_SERVER['HTTPS'] == 'on' || $_SERVER['HTTPS'] == 1) ||
+        isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&
+        $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'https'
+    ) {
+        $protocol = 'https://';
+        return str_replace('http://', $protocol, $url);
+    }
+    $protocol = 'http://';
+    return str_replace('https://', $protocol, $url);
 }
