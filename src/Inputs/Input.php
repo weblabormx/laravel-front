29372a614b91549794591a931bf597521b329a31
Luan Himmlisch <luan@himmlisch.com.mx>
2024-09-07 01:26:01 -0600

    Made input Htmlable

--- src/Inputs/Input.php
+++ src/Inputs/Input.php
@@ -2,6 +2,7 @@
 
 namespace WeblaborMx\Front\Inputs;
 
+use Illuminate\Contracts\Support\Htmlable;
 use WeblaborMx\Front\Front;
 use WeblaborMx\Front\Traits\InputVisibility;
 use WeblaborMx\Front\Traits\InputSetters;
@@ -9,7 +10,7 @@ use WeblaborMx\Front\Traits\InputRules;
 use WeblaborMx\Front\Traits\WithWidth;
 use Illuminate\Support\Str;
 
-class Input
+class Input implements Htmlable
 {
 	use InputVisibility, InputSetters, InputRules, WithWidth;
 
@@ -23,7 +24,7 @@ class Input
 	public $needs_to_be_on_panel = true;
 	public $column;
 	public $extra;
-	public $source; 
+	public $source;
 	public $value;
 	public $size;
 	public $input_formatted = true;
@@ -32,7 +33,7 @@ class Input
 
 	public function __construct($title = null, $column = null, $extra = null, $source = null)
 	{
-		if(is_string($title)) {
+		if (is_string($title)) {
 			$this->title = __($title);
 		} else {
 			$this->title = $title;
@@ -44,15 +45,15 @@ class Input
 		$this->load();
 	}
 
-	public static function make($title = null, $column = null, $extra = null) 
+	public static function make($title = null, $column = null, $extra = null)
 	{
-		if(is_null($column) && !is_null($title) && is_string($title)) {
+		if (is_null($column) && !is_null($title) && is_string($title)) {
 			$column = class_basename($title);
 			$column = Str::snake($column);
 		}
 
 		$source = session('source');
-		return new static($title, $column, $extra, $source);	
+		return new static($title, $column, $extra, $source);
 	}
 
 	public function load()
@@ -62,7 +63,7 @@ class Input
 
 	public function setValue($value)
 	{
-		if(!is_string($value) && is_callable($value)) {
+		if (!is_string($value) && is_callable($value)) {
 			$value = $value();
 		}
 		$this->value = $value;
@@ -72,22 +73,22 @@ class Input
 
 	public function getValue($object)
 	{
-		if(isset($this->value)) {
+		if (isset($this->value)) {
 			return $this->value;
 		}
-		if(!isset($object)) {
+		if (!isset($object)) {
 			return;
 		}
 		$return = '';
 		$column = $this->column;
-		if(!is_string($column) && is_callable($column)) {
+		if (!is_string($column) && is_callable($column)) {
 			$return = $column($object);
-		} else if(Str::contains($column, '.')) {
-			$return = collect(explode('.', $column))->reduce(function($carry, $item) use ($object) {
-				if(isset($carry[$item])) {
+		} else if (Str::contains($column, '.')) {
+			$return = collect(explode('.', $column))->reduce(function ($carry, $item) use ($object) {
+				if (isset($carry[$item])) {
 					return $carry[$item];
 				}
-				if(is_object($carry) && isset($carry->$item)) {
+				if (is_object($carry) && isset($carry->$item)) {
 					return $carry?->$item;
 				}
 				return null;
@@ -101,39 +102,39 @@ class Input
 		} catch (\TypeError $th) {
 			throw new \TypeError("Column '$column' can't be casted to string to be shown on input", 0, $th);
 		}
-		
-		$return = isset($return) && strlen($return)>0 ? $return : '--';
+
+		$return = isset($return) && strlen($return) > 0 ? $return : '--';
 		return $return;
 	}
 
 	public function getValueProcessed($object)
 	{
 		$return = $this->getValue($object);
-		if($return!='--' && isset($this->format)) {
+		if ($return != '--' && isset($this->format)) {
 			$format = $this->format;
 			$return = $format($return);
 		}
-		
-		if(Str::startsWith($return, 'http') && !isset($this->link)) {
+
+		if (Str::startsWith($return, 'http') && !isset($this->link)) {
 			$this->link = $return;
 			$this->link_target = '_blank';
 		}
 		$link = $this->link;
-		if(isset($link) && $return!='--') {
-			$add = isset($this->link_target) ? ' target="'.$this->link_target.'"' : '';
+		if (isset($link) && $return != '--') {
+			$add = isset($this->link_target) ? ' target="' . $this->link_target . '"' : '';
 			$return = "<a href='{$link}'{$add}>{$return}</a>";
 		}
-		if(isset($this->display_using) && is_callable($this->display_using) && $return!='--') {
+		if (isset($this->display_using) && is_callable($this->display_using) && $return != '--') {
 			$function = $this->display_using;
 			$return = $function($return);
 		}
 		return $return;
 	}
 
-	public function getColumn() 
+	public function getColumn()
 	{
 		$column = $this->column;
-		if(!Str::contains($column, '.')) {
+		if (!Str::contains($column, '.')) {
 			return $column;
 		}
 
@@ -155,15 +156,20 @@ class Input
 
 	public function formHtml()
 	{
-		if( $this->hide && (request()->filled($this->column))) {
+		if ($this->hide && (request()->filled($this->column))) {
 			return $this->hideForm();
 		}
-		if( !$this->input_formatted ) {
+		if (!$this->input_formatted) {
 			return $this->form();
 		}
 		$input = $this;
 		$html = view('front::input-form', compact('input'))->render();
-		return $this->form_before.$html.$this->form_after;
+		return $this->form_before . $html . $this->form_after;
+	}
+
+	public function toHtml()
+	{
+		return $this->formHtml();
 	}
 
 	public function showHtml($object)
@@ -188,17 +194,17 @@ class Input
 
 	public function size($size = null)
 	{
-		if(isset($this->attributes['style']) || is_null($size)) {
+		if (isset($this->attributes['style']) || is_null($size)) {
 			return $this;
 		}
 		$this->size = $size;
-		$this->attributes['style'] = 'width: '.$size.'px';
+		$this->attributes['style'] = 'width: ' . $size . 'px';
 		return $this;
 	}
 
 	public function massiveSize($size = null)
 	{
-		if(\Cache::store('array')->get('is_massive')!==true) {
+		if (\Cache::store('array')->get('is_massive') !== true) {
 			return $this;
 		}
 		return $this->size($size);
@@ -207,12 +213,12 @@ class Input
 	// In case there default attributes for the model
 	public function setDefaultValueFromAttributes($model)
 	{
-		if($this->source!='create' || !is_null($this->default_value) || is_null($model)) {
+		if ($this->source != 'create' || !is_null($this->default_value) || is_null($model)) {
 			return $this;
 		}
 		$model = new $model;
 		$attributes = $model->getAttributes();
-		if(isset($attributes[$this->column])) {
+		if (isset($attributes[$this->column])) {
 			$this->default($attributes[$this->column]);
 		}
 		return $this;
@@ -278,7 +284,7 @@ class Input
 					$return = $return->__toString();
 				} else if ($return instanceof \BackedEnum) {
 					$return = $return->value;
-				} else if($return instanceof \JsonSerializable){
+				} else if ($return instanceof \JsonSerializable) {
 					$return = json_encode($return);
 				}
 			}
