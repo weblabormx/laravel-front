8074032f37c707900be70c5fcf98d4a54191873b
Carlos Escobar <skalero01@gmail.com>
2024-10-11 16:07:02 -0600

    fix heklpers

--- src/Http/helpers.php
+++ src/Http/helpers.php
@@ -44,7 +44,7 @@ function getButtonByName($name, $front = null, $object = null)
     $config = config('front.buttons.' . $name);
     $extra = '';
     if ($name == 'delete') {
-        $extra = "data-type='confirm' title='" . __('Delete') . "' data-info='" . __('Do you really want to remove this item?') . "' data-button-yes='" . __('Yes') . "' data-button-no='" . __('No') . "' data-action='" . url($front->getBaseUrl() . '/' . $object->getKey()) . "' data-redirection='" . url($front->getBaseUrl()) . "' data-variables='{ \"_method\": \"delete\", \"_token\": \"" . csrf_token() . "\" }'";
+        $extra = "data-type='confirm' title='" . __('Delete') . "' data-info='" . __('Do you really want to remove this item?') . "' data-button-yes='" . __('Yes') . "' data-button-no='" . __('No') . "' data-action='" . url($front->getBaseUrl() . '/' . $object->getKey()) . "' data-redirection='" . url()->full() . "' data-variables='{ \"_method\": \"delete\", \"_token\": \"" . csrf_token() . "\" }'";
     }
     return Button::make($config['name'])
         ->setIcon($config['icon'])
