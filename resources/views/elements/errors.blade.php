b5803ba95afe46299f4fa75ef504106c4dec9233
Carlos Escobar <carlosescobar@weblabor.mx>
2025-08-22 11:04:13 -0600

    flash not used?

--- resources/views/elements/errors.blade.php
+++ resources/views/elements/errors.blade.php
@@ -1,3 +1,4 @@
+@include('flash::message')
 @if($errors->any())
     <div class="text-sm text-red-700 rounded-md bg-red-50 p-4 my-4">
         {{ __('Next errors were gotten:') }} {{ collect($errors->all())->implode(', ')}}
