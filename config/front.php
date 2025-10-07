b7db77686c456f2ff323d33a037ec1c22af368cf
Carlos Escobar <carlosescobar@weblabor.mx>
2025-10-07 16:18:32 -0600

    improve default config class
    bg white and focus with primary

--- config/front.php
+++ config/front.php
@@ -167,7 +167,7 @@ return [
     */
 
     'default_input_attributes' => [
-        'class' => 'mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm'
+        'class' => 'mt-1 block w-full rounded-md border border-gray-300 py-2 px-3 shadow-sm bg-white focus:border-primary-500 focus:outline-none focus:ring-primary-500 sm:text-sm'
     ],
 
     /* 
