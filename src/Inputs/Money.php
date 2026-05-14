bd5e29a10195fdabc41ecb57dc8ea6e26ddcf0e5
Carlos Escobar <carlosescobar@weblabor.mx>
2026-05-14 15:40:54 -0600

    add decimals on moneyt

--- src/Inputs/Money.php
+++ src/Inputs/Money.php
@@ -4,15 +4,21 @@ namespace WeblaborMx\Front\Inputs;
 
 class Money extends Input
 {
+    public $decimals = 2;
+
     public function form()
     {
-        $this->attributes['step'] = '.01';
-
+        $this->attributes['step'] = $this->getStep();
         $input = html()
             ->number($this->getColumn(), $this->getDefaultValue())
             ->attributes($this->attributes);
 
         return InputGroup::make('$', $input)->form();
+    }
+
+    protected function getStep()
+    {
+        return 1 / (10 ** $this->decimals);
     }
 
     public function getValue($object)
@@ -21,6 +27,12 @@ class Money extends Input
         if (!is_numeric($value)) {
             return $value;
         }
-        return '$' . number_format($value, 2);
+        return '$' . number_format($value, $this->decimals);
+    }
+
+    public function decimals($decimals)
+    {
+        $this->decimals = $decimals;
+        return $this;
     }
 }
